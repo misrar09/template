b96826ef454e944a96732e3647aa8e2a0eeb49b4
misrar09 <133796840+misrar09@users.noreply.github.com>
2023-12-11 12:25:56 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faHome);
+
+window.vue = {};
+window.vue.App = createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app');
